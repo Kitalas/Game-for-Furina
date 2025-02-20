44e85c2917b9ed3674b6310a5b7aa8cb1c6a2e33
MKhoroshko <margo.khoroshko@gmail.com>
2025-02-20 19:27:01 +0200

    commit forst

---
+++ xx(02(25/14.py
@@ -0,0 +1,13 @@
+# import re
+#
+# email = input('Enter your email: ')
+#
+# patter = f'\w+@[a-z0-9]+\.[a-z0-9]+$'
+# match_result = re.match(patter, email)
+#
+# if match_result is not None:
+#     print(match_result.groups())
+#     print('Valid email')
+# else:
+#     print('Invalid')
+#
---
+++ xx(02(25/19.py
@@ -0,0 +1,29 @@
+import time
+
+
+def red_lingh_decorator(func):
+    def wrapper():
+        print(colorama.Fore.RED)
+        func()
+        print(colorama.Fore.RESET)
+    return wrapper
+
+
+def green_lingh_decorator(func):
+    def wrapper(*args, *kwargs):
+        start_time = time.time()
+        result = func(*args, *kwargs)
+        end_time = time.time()
+        print(f"Час исполнение игры: {end_time - start_time} сек")
+    return wrapper
+
+    @timer_decorator
+    def example_function(n):
+        total = 0
+        for i in range(n):
+            total += i
+        return total
+
+
+result = example_function(10653284)
+print(result)
